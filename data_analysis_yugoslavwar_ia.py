781e69d9424ca6c72aabf61f9656ab8226f24bec
Gudmundur Haraldsson <gummihal@gmail.com>
2020-01-16 15:23:36 +0100

    Selecting preliminary data (country, deaths)

--- data_analysis_yugoslavwar_ia.py
+++ data_analysis_yugoslavwar_ia.py
@@ -1,4 +1,14 @@
 import json
 
+# Loading the json data
 with open('conflict_data_full_lined.json', encoding='utf8') as file:
     data = json.load(file)
+
+total_deaths = 0
+    for datum in data:
+        if 'Yugoslavia' in datum['country'] and datum['year'] == 1991:
+            country = datum['country']
+            deaths = datum['best']
+            #print(deaths)
+            total_deaths = total_deaths + datum['best']
+            #print(f'Number of deaths {datum["best"]}')
