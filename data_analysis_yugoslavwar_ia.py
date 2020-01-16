68929c8795eaa7906759a1d06715c3a4ef5b8fae
Gudmundur Haraldsson <gummihal@gmail.com>
2020-01-16 15:30:26 +0100

    Creating a preliminary csv file (country, death)

--- data_analysis_yugoslavwar_ia.py
+++ data_analysis_yugoslavwar_ia.py
@@ -5,10 +5,17 @@ with open('conflict_data_full_lined.json', encoding='utf8') as file:
     data = json.load(file)
 
 total_deaths = 0
+with open('yugoslavia_death_data.csv', 'w', encoding='utf8') as file:
     for datum in data:
         if 'Yugoslavia' in datum['country'] and datum['year'] == 1991:
             country = datum['country']
             deaths = datum['best']
             #print(deaths)
             total_deaths = total_deaths + datum['best']
-            #print(f'Number of deaths {datum["best"]}')
+            #print(f'Number of deaths {datum["best"]}')
+
+            # Converting json data to csv
+            file.write(f'{country},{deaths}\n')
+    
+
+
