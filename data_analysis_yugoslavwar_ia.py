de08b4bb61fed84ff8d2b214753cb6abebc69dd9
Gudmundur Haraldsson <gummihal@gmail.com>
2020-01-17 11:45:17 +0100

    Adding 1992 and calculating total deaths in 1991-1992 (part 2)

--- data_analysis_yugoslavwar_ia.py
+++ data_analysis_yugoslavwar_ia.py
@@ -9,16 +9,16 @@ total_deaths_1992 = 0
 with open('yugoslavia_death_data.csv', 'w', encoding='utf8') as file:
     file.write('deaths,date,\n')
     for datum in data:
-        if 'Yugoslavia' in datum['country'] and datum['year'] == 1992:
+        if 'Yugoslavia' in datum['country'] and (datum['year'] == 1991 or datum['year'] == 1992):
             country = datum['country']
             deaths = datum['best']
             #print(deaths)
             #total_deaths_1991 = total_deaths_1991 + datum['best']
-            total_deaths_1992 = total_deaths_1992 + datum['best']
-            print(total_deaths_1992)
+            # total_deaths_1992 = total_deaths_1992 + datum['best']
+            # print(total_deaths_1992)
 
             # Converting json data to csv
-           # file.write(f"{deaths},{datum['date_start']}\n")
+            file.write(f"{deaths},{datum['date_start']}\n")
     
 
 
