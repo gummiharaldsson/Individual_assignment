518f4d87a0b942e47493d09b60e200624b5be625
Gudmundur Haraldsson <gummihal@gmail.com>
2020-01-17 12:36:34 +0100

    Making a new csv file based on year

--- data_analysis_yugoslavwar_ia.py
+++ data_analysis_yugoslavwar_ia.py
@@ -6,19 +6,20 @@ with open('conflict_data_full_lined.json', encoding='utf8') as file:
 
 total_deaths = 0
 total_deaths_1992 = 0
-with open('yugoslavia_death_data.csv', 'w', encoding='utf8') as file:
-    file.write('deaths,date,\n')
+with open('yugoslavia_death_data_sep.csv', 'w', encoding='utf8') as file:
+    file.write('deaths,1991,1992,\n')
     for datum in data:
         if 'Yugoslavia' in datum['country'] and (datum['year'] == 1991 or datum['year'] == 1992):
             country = datum['country']
             deaths = datum['best']
+          
             #print(deaths)
             #total_deaths_1991 = total_deaths_1991 + datum['best']
             # total_deaths_1992 = total_deaths_1992 + datum['best']
             # print(total_deaths_1992)
 
             # Converting json data to csv
-            file.write(f"{deaths},{datum['date_start']}\n")
+            file.write(f"{deaths},{datum['year'] == 1991},{datum['year'] == 1992}\n")
     
 
 
